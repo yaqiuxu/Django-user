5bfe172c9c0e8cef0fae36eacec60b618c3865e7
Yaqiu Xu <yaqiuxu@usc.edu>
2023-08-15 12:36:01 -0700

    update settings

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "user.middleware.auth.AuthMiddleware",
 ]
 
 ROOT_URLCONF = "myProject.urls"
---
+++ myProject/user/middleware/auth.py
@@ -0,0 +1,21 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.http import JsonResponse
+
+class AuthMiddleware(MiddlewareMixin):
+
+    def process_request(self, request):
+        
+        print("request.path_info", request.path_info)
+        # 0. the pages that need no authentication
+        if request.path_info in ["/login/", "/userLogin/", "/register/", "/registerUser/"]:
+            return
+
+        # 1. read current user's session. If session exists, then the user is already logged in
+        user_info = request.session.get("info")
+        print("user_info", user_info)
+        if user_info:
+            return
+
+        # 2. Block the user if it is not logged in
+        return JsonResponse({'success': False, 'message': 'please login.'})
+    
